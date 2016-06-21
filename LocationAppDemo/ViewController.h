57267f8249d3523ce56a52273b35f0ad20b608a8
vm mac <vmmac@vms-mac.local>
2016-06-21 14:30:34 +0530

    Initial Commit

---
+++ LocationAppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationAppDemo
+//
+//  Created by vm mac on 21/06/2016.
+//  Copyright © 2016 PytenLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationAppDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationAppDemo
+//
+//  Created by vm mac on 21/06/2016.
+//  Copyright © 2016 PytenLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
